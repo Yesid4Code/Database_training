c58cc3c08d9ba2d1d8cb0efa39d6018a256c97a7
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2021-05-31 00:56:22 -0500

    Query of a list of name that not start with a vowel

---
+++ 0x01_SQL_practice_HackerRank/0x13_weather_observation_station_9.sql
@@ -0,0 +1,26 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+Input Format
+The STATION table is described as follows:
+_____________________________
+|           STATION         |
+-----------------------------
+| FIELD      | TYPE         |
+-----------------------------
+| ID         | NUMBER       |
+-----------------------------
+| CITY       | VARCHAR2(21) |
+-----------------------------
+| STATE      | VARCHAR2(2)  |
+-----------------------------
+| LAT_N      | NUMBER       |
+-----------------------------
+| LONG_W     | NUMBER       |
+-----------------------------
+
+Where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+/* Options */
+SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '^[aeiou]';
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[^aeiou]';
